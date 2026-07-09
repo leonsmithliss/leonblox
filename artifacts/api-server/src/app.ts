bdf2bf487386e36f3662b42b9ffe656ab6244574
leonsmithliss <leonsmithliss@gmail.com>
2026-07-09 16:21:43 -0400

    Fix Express 5 wildcard route syntax

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -57,7 +57,7 @@ app.use("/api", router);
 
 const staticDir = path.resolve(__dirname, "../../leonblox/dist/public");
 app.use(express.static(staticDir));
-app.get("*", (_req, res) => {
+app.get("/*path", (_req, res) => {
   res.sendFile(path.join(staticDir, "index.html"));
 });
 
